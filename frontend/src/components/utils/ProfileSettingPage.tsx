86aaec5287ad90840efbc513472ddfb23ccc556b
taisei08 <4122168a@g.hit-u.ac.jp>
2023-11-17 23:49:16 +0900

    [update]初期アイコン設定画面の修正

--- frontend/src/components/utils/ProfileSettingPage.tsx
+++ frontend/src/components/utils/ProfileSettingPage.tsx
@@ -1,19 +1,35 @@
-import React from "react";
+import { useState } from "react";
 import Button from "@material-ui/core/Button";
 
 const ProfileSettingPage = (props) => {
 
   const { userData, setUserData, onNext } = props;
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
+  const [school, setsc] = useState("");
   const [passwordConfirmation, setPasswordConfirmation] = useState("");
-  const [alertMessageOpen, setAlertMessageOpen] = useState<boolean>(false)
 
 
   return (
     <div>
-      <p>誕生日や学校情報を設定しましょう！</p>
+      <p>通っている(卒業した)学校の名前などを入力</p>
       {/* フォームをここに追加 */}
+      <TextField
+        variant="outlined"
+        required
+        fullWidth
+        label="学校名"
+        value={email}
+        margin="dense"
+        onChange={(event) => setEmail(event.target.value)}
+      />
+            <TextField
+        variant="outlined"
+        required
+        fullWidth
+        label="学部学科"
+        value={email}
+        margin="dense"
+        onChange={(event) => setEmail(event.target.value)}
+      />
       <Button variant="outlined" color="primary" onClick={() => console.log("保存")}>
         保存
       </Button>
